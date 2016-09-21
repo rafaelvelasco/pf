a08221cece72ce13fc578b325d7a6d186bbfafaa
Rodrigo Rocha Gomes e Souza <rodrigorgs@gmail.com>
2016-09-21 16:17:44 -0300

    Itera sobre todos os merges.

--- pf/src/main/java/Analisador.java
+++ pf/src/main/java/Analisador.java
@@ -3,25 +3,26 @@ import java.io.IOException;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRefNameException;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 
 public class Analisador {
-	
-	 public static void main(String[] args) throws IOException, InvalidRefNameException, GitAPIException {
-	        try (Repository repository = CookbookHelper.openJGitCookbookRepository()) {
-	            try (Git git = new Git(repository)) {
-	                Iterable<RevCommit> commits = git.log().all().call();
-	                int count = 0;
-	                for (RevCommit commit : commits) {
-	                    System.out.println("LogCommit: " + commit);
-	                    count++;
-	                }
-	                System.out.println(count);
-	            }
-	        }
-	    }
 
-	   
+	public static void main(String[] args) throws IOException, InvalidRefNameException, GitAPIException {
+		Repository repository = new FileRepository("/home/rodrigorgs/local/git/RxJava/.git");
+
+		Git git = new Git(repository);
+		Iterable<RevCommit> commits = git.log().all().call();
+		int count = 0;
+		for (RevCommit commit : commits) {
+			if (commit.getParentCount() == 2) {
+				System.out.println("LogCommit: " + commit.getShortMessage());
+				count++;
+			}
+		}
+		System.out.println(count);
+		git.close();
+	}
+
 }
-	
